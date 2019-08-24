601c4f88c8eca4ea0b526bc45a307556f0f013d4
Mamun <abdullahalmamun914@gmail.com>
2019-08-24 09:17:11 +0600

    add some style

--- src/components/TempResult.js
+++ src/components/TempResult.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const TempResult = (props) => {
     return (
-        <div>
-            {props.celsius>=100?<h5>The Water is boiling....</h5>:<h5>The water is not boiling yet</h5>}
+        <div style={{padding:'10px',fontSize:'40px'}}>
+            {props.celsius>=100?<h5 style={{fontSize:'40px'}}>>The Water is boiling....</h5>:<h5>The water is not boiling yet</h5>}
         </div>
     );
 };
