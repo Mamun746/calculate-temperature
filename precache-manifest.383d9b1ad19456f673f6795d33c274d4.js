58a3a8aee6377f28c5258fe684b1f5b109c7bdc6
Mamun <abdullahalmamun914@gmail.com>
2019-08-24 09:52:43 +0600

    Updates

---
+++ precache-manifest.383d9b1ad19456f673f6795d33c274d4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29376454003dfff9737d729f01bd3be",
+    "url": "/Mamun746/calculate-temperature/index.html"
+  },
+  {
+    "revision": "ff3468f0640513878add",
+    "url": "/Mamun746/calculate-temperature/static/js/2.ce9cbc03.chunk.js"
+  },
+  {
+    "revision": "891ecbde04c8d001b549",
+    "url": "/Mamun746/calculate-temperature/static/js/main.bcf2dc34.chunk.js"
+  },
+  {
+    "revision": "e658217bf58224567ffb",
+    "url": "/Mamun746/calculate-temperature/static/js/runtime~main.7dd3663a.js"
+  }
+]);
